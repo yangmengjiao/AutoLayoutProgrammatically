f266af3c85ce519f4cf8ce6b743da9e6a81acce6
yangmengjiao <mengjiaoshun@gmail.com>
2021-01-27 18:04:34 -0600

    init

--- DeleteThisToo/ViewController.swift
+++ DeleteThisToo/ViewController.swift
@@ -1,6 +1,5 @@
 //
 //  ViewController.swift
-//  DeleteThisToo
 //
 //  Created by mengjiao on 1/27/21.
 //
@@ -8,12 +7,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var image: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "Canary.jpg")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var nameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "My name is Canary"
+        // translatesAutoresizingMaskIntoConstraints must set to be False
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
+        view.backgroundColor = .white
     }
 
+    //MARK：- set up views
+    func setupViews() {
+        view.addSubview(nameLabel)
+        view.addSubview(image)
+       
+    }
+    
+    //MARK：- set up constraints
+    func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        //image
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            image.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            image.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+          
+        ])
+        
+        //nameLabel
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: image.bottomAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameLabel.widthAnchor.constraint(equalToConstant: 300),
+            nameLabel.heightAnchor.constraint(equalToConstant: 50),
+            nameLabel.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+            
+        ])
+            
+    }
 
 }
 
